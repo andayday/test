d84c83fdb339b13b9db7e52e4c1dc4aaa61ea525
anday <andayup_108@163.com>
2018-11-07 11:14:07 +0800

    debug

--- simpledu/simpledu/handlers/front.py
+++ simpledu/simpledu/handlers/front.py
@@ -1,9 +1,12 @@
 from flask import Blueprint, render_template
 from simpledu.models import Course
+from simpledu.models import User
 
 front = Blueprint('front', __name__)
 
 @front.route('/')
 def index():
+    userinfo = User.query.get(username = "admin")
+    print("userinfo : ", userinfo)
     courses = Course.query.all()
     return render_template('index.html', courses = courses)
