60331b60cf3f59ba05951c177c7a45b879d00c12
anday <andayup_108@163.com>
2018-11-07 15:56:58 +0800

    models.py

--- simpledu/simpledu/models.py
+++ simpledu/simpledu/models.py
@@ -4,23 +4,26 @@ from flask_sqlalchemy import SQLAlchemy
 #db = SQLAlchemy(app)
 db = SQLAlchemy()
 
-class User(db.Model):
+class Base(db.Model):
+    __abstract__ = True
+
+    created_at = db.Column(db.DateTime, default = datetime.utcnow)
+    updated_at = db.Column(db.DateTime, default = datetime.utcnow, onupdate = datetime.utcnow)
+
+
+class User(Base):
     __tablename__ = 'user'
     
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(32), unique = True, index = True, nullable = False)
     publish_courses = db.relationship('Course')
-    created_at = db.Column(db.DateTime, default = datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default = datetime.utcnow, onupdate = datetime.utcnow)
 
-class Course(db.Model):
+class Course(Base):
     __tablename__ = 'course'
 
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(128), unique = True, index = True, nullable = False)
     author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete = 'CASCADE'))
     author = db.relationship('User', uselist = False)
-    created_at = db.Column(db.DateTime, default = datetime.utcnow)
-    updated_at = db.Column(db.DateTime, default = datetime.utcnow, onupdate = datetime.utcnow)
     
 
