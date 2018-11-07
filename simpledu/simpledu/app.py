b8a5941a86dce9e4d03fcbe1655d83191789ff62
anday <andayup_108@163.com>
2018-11-07 16:32:43 +0800

    modify

--- simpledu/simpledu/app.py
+++ simpledu/simpledu/app.py
@@ -1,4 +1,5 @@
 from flask import Flask , render_template
+from flask_migrate import Migrate
 from simpledu.config import configs
 from simpledu.models import db, Course
 
@@ -17,6 +18,7 @@ def create_app(config):
     app = Flask(__name__)
     app.config.from_object(configs.get(config))
     db.init_app(app)
+    Migrate(app, db)
     register_blueprint(app)
 
     return app
