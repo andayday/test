6cb9073b4fcf6c2d770da209fd6ba5c97e277a7f
anday <andayup_108@163.com>
2018-11-08 18:10:54 +0800

    modify password

--- simpledu/simpledu/forms.py
+++ simpledu/simpledu/forms.py
@@ -6,7 +6,7 @@ from simpledu.models import db, User
 class RegisterForm(FlaskForm):
     username = StringField('Username', validators = [Required(), Length(3, 24)])
     email = StringField('Email', validators = [Required(), Email()])
-    password = PasswordField('Password', validators = [Required(), Length(6, 24)])
+    password = PasswordField('Password', validators = [Required(), Length(6, 128)])
     repeat_password = PasswordField('Password again', validators = [Required(), EqualTo('password')])
     submit = SubmitField('Submit')
 
