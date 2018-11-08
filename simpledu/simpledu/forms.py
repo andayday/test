bc3831c05571cf20370355841f5a5a349ba60b41
anday <andayup_108@163.com>
2018-11-08 16:27:03 +0800

    add register function

--- simpledu/simpledu/forms.py
+++ simpledu/simpledu/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import Length, Email, EqualTo, Required
+from simpledu.models import db, User
 
 class RegisterForm(FlaskForm):
     username = StringField('用户名', validators = [Required(), Length(3, 24)])
@@ -8,6 +9,15 @@ class RegisterForm(FlaskForm):
     password = PasswordField('密码', validators = [Required(), Length(6, 24)])
     repeat_password = PasswordField('重复密码', validators = [Required(), EqualTo('password')])
     submit = SubmitField('提交')
+
+    def create_user(self):
+        user = User()
+        user.username = self.username.data
+        user.email = self.email.data
+        user.password = self.password.data
+        db.session.add(user)
+        db.session.commit()
+        return user
 
 
 class LoginForm(FlaskForm):
