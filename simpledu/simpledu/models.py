531e01eeb076d485436bc02fa33643124f7ea840
anday <andayup_108@163.com>
2018-11-10 16:28:28 +0800

    add datas

--- simpledu/simpledu/models.py
+++ simpledu/simpledu/models.py
@@ -59,7 +59,8 @@ class Course(Base):
     __tablename__ = 'course'
 
     id = db.Column(db.Integer, primary_key = True)
-    name = db.Column(db.String(128), unique = True, index = True, nullable = False)
+    name = db.Column(db.String(128), index = True, nullable = False)
+    #name = db.Column(db.String(128), unique = True, index = True, nullable = False)
     description = db.Column(db.String(256))
     image_url = db.Column(db.String(256))
     author_id = db.Column(db.Integer, db.ForeignKey('user.id', ondelete = 'CASCADE'))
