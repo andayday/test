00f7a489f76f2f96edd2896bf77c358b9c39400f
anday <andayup_108@163.com>
2018-11-10 11:55:33 +0800

    add datas

--- simpledu/scripts/crawl_shiyanlou_courses.py
+++ simpledu/scripts/crawl_shiyanlou_courses.py
@@ -1,19 +1,18 @@
 import scrapy
 
 class CourseSpider(scrapy.Spider):
-    name = 'course'
+    name = 'courses'
 
     start_urls = ['https://www.shiyanlou.com/bootcamp/']
 
 
     def parse(self, response):
         for course in response.css('div.bootcamp-courses-item'):
-            print(course)
             yield {
                     'name': course.xpath('.//div[@class="course-title"]/a/span/text()').extract_first().strip(),
                     'description': course.xpath('.//div[@class="course-desc"]/a/p/text()').extract_first().strip(),
                     'image_url': course.xpath('.//div[@class="course-img"]/a/img/@src').extract_first()
-                    }
+                }
             
 
 
