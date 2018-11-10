2e89b886a4c3f51e4abb4dbc553356d6461347f0
anday <andayup_108@163.com>
2018-11-10 17:44:25 +0800

    add datas

--- simpledu/simpledu/models.py
+++ simpledu/simpledu/models.py
@@ -2,6 +2,7 @@ from datetime import datetime
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
+from flask import url_for
 
 #db = SQLAlchemy(app)
 db = SQLAlchemy()
