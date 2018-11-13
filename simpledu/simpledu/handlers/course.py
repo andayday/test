b2420dd91fd80da7b80d5a6bb1ceeb20f6bf0faa
anday <andayup_108@163.com>
2018-11-13 16:55:20 +0800

    modify page function

--- simpledu/simpledu/handlers/course.py
+++ simpledu/simpledu/handlers/course.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
-from simpledu.models import Course
+from simpledu.models import Course, Chapter
+from flask_login import login_required
 
 course = Blueprint('course', __name__, url_prefix = '/courses')
 
@@ -9,6 +10,10 @@ def detail(course_id):
     return render_template('course/detail.html', course = course)
 
 
-
+@course.route('/<int:course_id>/chapters/<int:chapter_id>')
+@login_required
+def chapter(course_id, chapter_id):
+    chapter = Chapter.query.get_or_404(chapter_id)
+    return render_template('course/chapter.html', chapter = chapter)
 
 
