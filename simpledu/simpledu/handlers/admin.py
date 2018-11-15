485aa8e8e7abb20ed4d5de72312c57aaf902625c
anday <andayup_108@163.com>
2018-11-15 15:39:41 +0800

    mofiy admin

--- simpledu/simpledu/handlers/admin.py
+++ simpledu/simpledu/handlers/admin.py
@@ -1,8 +1,22 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from simpledu.decorators import admin_required
 
 admin = Blueprint('admin', __name__, url_prefix = '/admin')
 
 @admin.route('/')
+@admin_required
 def index():
-    return 'admin'
+    return render_template('admin/index.html')
+
+
+@admin.route('/users')
+def users():
+    page = request.args.get('page', default = 1, type = int)
+    pagination = User.query.paginate(
+            page = page,
+            per_page = current_app.config['ADMIN_PER_PAGE'],
+            error_out = False,
+            )
+    return render_template('admin/users.html', pagination = pagination)
+    
 
