22e57bb594511f4f238b8a14b23671ce6bca3e43
anday <andayup_108@163.com>
2018-11-23 09:39:32 +0800

    modify

--- simpledu/manage.py
+++ simpledu/manage.py
@@ -5,7 +5,7 @@ app = create_app('development')
 if __name__ == '__main__':
     from gevent import pywsgi
     from geventwebsocket.handler import WebSocketHandler
-    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class=WebSocketHandler)
+    server = pywsgi.WSGIServer(('0.0.0.0', 5000), app, handler_class = WebSocketHandler)
     server.server_forever()
     #app.run()
 
