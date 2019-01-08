79b2646e32cf7c85af75594a71e97c32d71e9c49
anday <andayup_108@163.com>
2019-01-08 09:58:06 +0800

    modify

--- jobplus/jobplus/handlers/front.py
+++ jobplus/jobplus/handlers/front.py
@@ -14,36 +14,36 @@ def index():
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-            user = User.query.filter_by(email = form.email.data).first()
-            login_user(user, form.remember_me.data)
-            next = 'user.profile'
-            if user.is_admin:
-                    next = 'admin.index'	
-            elif user.is_company:
-                    next = 'company.profile'
-            return redirect(url_for(next))
+        user = User.query.filter_by(email = form.email.data).first()
+        login_user(user, form.remember_me.data)
+        next = 'user.profile'
+        if user.is_admin:
+            next = 'admin.index'	
+        elif user.is_company:
+            next = 'company.profile'
+        return redirect(url_for(next))
     return render_template('login.html', form = form)
 
 @front.route('/userregister', methods = ['GET', 'POST'])
 def userregister():
-	form = RegisterForm()
-	if form.validate_on_submit():
-		form.create_user()
-		flash('register, success!','success')
-		return redirect(url_for('.login'))
-	return render_template('userregister.html', form = form)
+    form = RegisterForm()
+    if form.validate_on_submit():
+        form.create_user()
+        flash('register, success!','success')
+        return redirect(url_for('.login'))
+    return render_template('userregister.html', form = form)
 
 @front.route('/companyregister', methods = ['GET', 'POST'])
-	form = RegisterForm()
-	form.name.label = u'company label'
-	if form.validate_on_submit():
-		company_user = form.create_user()
-		company_user.role = User.ROLE_COMPANY
-		db.session.add(company_user)
-		db.session.commit()
-		flash('register success, please login in', 'success')
-		return redirect(url_for('.login'))
+    form = RegisterForm()
+    form.name.label = u'company label'
+    if form.validate_on_submit():
+        company_user = form.create_user()
+        company_user.role = User.ROLE_COMPANY
+        db.session.add(company_user)
+        db.session.commit()
+        flash('register success, please login in', 'success')
+        return redirect(url_for('.login'))
 
-	return render_template('companyregister.html', form = form)
+    return render_template('companyregister.html', form = form)
 	
 
