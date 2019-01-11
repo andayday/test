cb433f7227ed3b25143a9714442f016e2ba37ebc
anday <andayup_108@163.com>
2019-01-11 11:10:54 +0800

    add user.profile route

--- jobplus/jobplus/handlers/user.py
+++ jobplus/jobplus/handlers/user.py
@@ -1,4 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, redirect
+from flask_login import login_required
 
 user = Blueprint('user', __name__, url_prefix = '/user')
+
+@user.route('/profile', methods = ['GET', 'POST'])
+def profile():
+    return render_template('index.html')
+
 
